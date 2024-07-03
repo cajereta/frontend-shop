fc445cbd65fbda23ef7f8adcd60bcf8b491ce015
Emanuel Gomez <msijuicebox@gmail.com>
2024-07-03 19:53:05 -0300

    replaced images to use Nextjs ones

--- components/carousel/embla-carousel.tsx
+++ components/carousel/embla-carousel.tsx
@@ -5,6 +5,7 @@ import Autoplay from "embla-carousel-autoplay";
 
 import useEmblaCarousel from "embla-carousel-react";
 import "./carousel.css";
+import Image from "next/image";
 
 type carousel = {
   src: string;
@@ -36,9 +37,12 @@ export const EmblaCarousel: React.FC<PropType> = (props) => {
         <div className="embla__container">
           {slides.map((slide, index) => (
             <div className="embla__slide" key={index}>
-              <div
-                style={divStyle(slide.src)}
-                className="w-full h-[40rem] brightness-[0.7] bg-fit bg-center rounded-2xl"
+              <Image
+                src={slide.src}
+                alt={slide.alt}
+                className="w-full h-full object-cover brightness-[60%]"
+                width={1400}
+                height={800}
               />
               <div className="absolute top-[8rem] md:top-80 md:bottom-[10rem] p-8 md:p-16 w-full font-bold text-white [text-shadow:_0px_20px_20px_rgb(0_0_0_/_80%)]">
                 <h3 className="my-4 text-4xl md:text-7xl drop-shadow-lg tracking-wide ">
--- components/testimonials/Testimonials.tsx
+++ components/testimonials/Testimonials.tsx
@@ -5,7 +5,7 @@ export const Testimonials = () => {
     <div className="flex gap-6 my-10 px-12 md:px-14 xl:px-24 flex-col h-[40rem] md:flex-row w-full justify-center  ">
       <div className="flex flex-col w-full md:w-[30%] justify-center text-center h-full p-4 md:p-12 items-center gap-4 rounded-xl ring-gray-900/30 border ">
         <Image
-          src="/avatar.png"
+          src="/avatar.webp"
           width={64}
           height={64}
           className="rounded-full  mx-auto"
@@ -19,24 +19,24 @@ export const Testimonials = () => {
           "Lorem ipsum dolor sit amet consectetur adipisicing elit."
         </p>
       </div>
-      <div className="w-full md:w-[70%] h-full rounded-md">
-        <div
+      <div className="relative w-full md:w-[70%] h-full rounded-md">
+        <Image
+          src="/22.webp"
+          width={1400}
+          height={800}
+          alt="Testimonial"
           className="w-full h-full rounded-xl bg-cover"
-          style={{ backgroundImage: 'url("/22.jpg")' }}
-        >
-          <div className="flex w-full h-full bg-black/60 rounded-xl justify-center items-center [text-shadow:_0px_10px_10px_rgb(0_0_0_/_60%)]">
-            <div className="flex w-3/5 h-3/5 rounded-lg bg-white/30 items-center">
-              <div className="flex flex-col text-center gap-2 mx-auto items-center">
-                <button className="bg-black w-fit p-2  text-white font-bold border border-slate-800 rounded-md uppercase">
-                  25% discount
-                </button>
-                <h3 className="text-white text-2xl font-bold ">
-                  Summer Discount
-                </h3>
-                <p className="text-gray-100 font-medium">
-                  Starting from $19.99
-                </p>
-              </div>
+        />
+        <div className="absolute top-0 flex w-full h-full bg-black/60 rounded-xl justify-center items-center [text-shadow:_0px_10px_10px_rgb(0_0_0_/_60%)]">
+          <div className="flex w-3/5 h-3/5 rounded-lg bg-white/30 items-center">
+            <div className="flex flex-col text-center gap-2 mx-auto items-center">
+              <button className="bg-black w-fit p-2  text-white font-bold border border-slate-800 rounded-md uppercase">
+                25% discount
+              </button>
+              <h3 className="text-white text-2xl font-bold ">
+                Summer Discount
+              </h3>
+              <p className="text-gray-100 font-medium">Starting from $19.99</p>
             </div>
           </div>
         </div>
